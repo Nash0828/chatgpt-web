53d08c0e42e732b64145aab1775c827fd03a9c6d
Nash0828 <414397541@qq.com>
2025-09-05 18:28:02 +0800

    更新 test.vue

--- test.vue
+++ test.vue
@@ -1,46 +1,16 @@
 // vue.config.js
-const { defineConfig } = require('@vue/cli-service')
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  
-  // 方式1: 使用 chainWebpack (推荐)
-  chainWebpack: config => {
-    // 图片处理规则
+module.exports = {
+  chainWebpack: (config) => {
+    // 修改图片规则
     config.module
       .rule('images')
-      .test(/\.(png|jpe?g|gif|webp|svg)(\?.*)?$/)
+      .test(/\.(png|jpe?g|gif|webp|avif)(\?.*)?$/)
       .use('url-loader')
       .loader('url-loader')
       .tap(options => {
-        options.limit = 20 * 1024 // 20KB
-        options.fallback = {
-          loader: 'file-loader',
-          options: {
-            name: 'img/[name].[hash:8].[ext]',
-            esModule: false
-          }
-        }
-        return options
-      })
-  },
-
-  // 或者方式2: 使用 configureWebpack
-  // configureWebpack: {
-  //   module: {
-  //     rules: [
-  //       {
-  //         test: /\.(png|jpe?g|gif|webp|svg)(\?.*)?$/,
-  //         use: {
-  //           loader: 'url-loader',
-  //           options: {
-  //             limit: 20 * 1024,
-  //             name: 'img/[name].[hash:8].[ext]',
-  //             esModule: false
-  //           }
-  //         }
-  //       }
-  //     ]
-  //   }
-  // }
-})
+        // 修改 limit 参数为 20KB
+        options.limit = 20 * 1024; // 20KB
+        return options;
+      });
+  }
+};
